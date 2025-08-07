a43171993f407234de0d3d7907fda059027352f7
cursosbackend <cursosbackends@gmail.com>
2025-08-07 02:19:40 +0000

    test

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -1,9 +1,33 @@
 
 from django.urls import path 
-from .views import hola , listar_productos
+from .views import (
+    ProductoListView,
+    ProductoCreateView,
+    ProductoDetailView,
+    ProductoUpdateView,
+    ProductoDeleteView,
+    desafio,
+    base,
+    test,
+    nombre,
+    test2,
+    test3,
+    layout,
+    herencia
+)
 
 urlpatterns = [
-    path("", hola, name="hola"),
-    path("productos/", listar_productos, name="lista_productos")
-   
+    path("", ProductoListView.as_view(), name="lista_productos"),
+    path("crear", ProductoCreateView.as_view(), name="crear_producto"),
+    path("producto/detalle/<int:pk>", ProductoDetailView.as_view(), name="detail_producto"),
+    path("producto/actualizacion/<int:pk>", ProductoUpdateView.as_view(), name="update_producto"),
+    path("producto/borrar/<int:pk>", ProductoDeleteView.as_view(), name="borrar_producto"),
+    path("desafio/", desafio , name="desafio"),
+    path("base/", base , name="base"),
+    path("test/", test , name="test"),
+    path("nombre/", nombre , name="nombre"),
+    path("test2/",test2, name="test2"),
+    path("test3/", test3, name="test3"),
+    path("layout/", layout, name="layout"),
+    path("herencia/", herencia, name="herencia"),
 ]
--- mi_app/views.py
+++ mi_app/views.py
@@ -1,17 +1,91 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from .models import Producto
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.http import HttpResponse
+
 
 # Create your views here.
 
-def hola(request):
-    return render(request, "mi_app/hola.html")
+
+class ProductoListView(ListView):
+    model = Producto
+    template_name = "productos/producto_list.html"
+    context_object_name = "productos"
+
+class ProductoDetailView(DetailView):
+    model = Producto
+    template_name = "productos/producto_Detail.html"
+    context_object_name = "producto"
 
 
-def listar_productos(request):
-    productos = Producto.objects.all() #query
-    
+class ProductoCreateView(CreateView):
+    model = Producto
+    template_name = "productos/producto_form.html"
+    fields = ["nombre","descripcion","precio","stock","imagen"]
+    success_url = reverse_lazy("lista_productos")
 
-    return render(request, "mi_app/lista_de_productos.html",{"productos": productos})
+
+class ProductoUpdateView(UpdateView):
+    model = Producto
+    template_name = "productos/producto_form.html"
+    fields = ["nombre","descripcion","precio","stock","imagen"]
+    success_url = reverse_lazy("lista_productos")
+
+
+class ProductoDeleteView(DeleteView):
+    model = Producto
+    template_name = "productos/producto_confirm.html"
+    success_url = reverse_lazy("lista_productos")
+
+
+
+def desafio(request):
+
+    empleados=[
+            'María González',
+            'Carlos Rodríguez',
+            'Laura Sánchez',
+            'Miguel Díaz',
+            'Sofía Ramírez',
+            'David Fernández',
+            'Lucía Morales',
+            'José Castillo',
+            'Paula Herrera',]
+
+    return render(request, "desafio.html",{"empleados": empleados})
+
+
+def base(request):
+    return render(request, "base.html",{})
+
+def test(request):
+    return HttpResponse("<h1>empleados</h1>")
+
+
+
+def nombre(request):
+    return render(request,"nombre.html",{})
+
+
+def test2(request):
+    return render(request, "test2.html",{})
+
+
+def test3(request):
+    return render(request, "test2.html",{})
+
+
+
+def layout(request):
+    return render(request, "mi_app/layout.html",{})
+
+
+
+def herencia(request):
+    return render(request, "mi_app/herencia.html",{})
+
+
 
 
 
