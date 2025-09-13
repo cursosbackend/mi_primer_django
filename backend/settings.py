5d1dd6b1497a476991bcd4511aea69518589e334
cursosbackend <cursosbackends@gmail.com>
2025-09-12 22:41:24 -0300

    Update settings.py
    test 3

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,8 @@ ALLOWED_HOSTS = []
 
 CSRF_TRUSTED_ORIGINS = [
     "https://localhost:8000",
-    "https://mi-primer-django.onrender.com/"
+    "https://mi-primer-django.onrender.com/",
+    "http://mi-primer-django.onrender.com/"
 ]   
 
 
