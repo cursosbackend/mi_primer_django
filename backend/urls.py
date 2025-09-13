379adbec656a427cad2e3ed8c18ea654e9b2c558
cursosbackend <cursosbackends@gmail.com>
2025-09-12 22:54:00 -0300

    Update urls.py
    url

--- backend/urls.py
+++ backend/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("productos/", include("mi_app.urls")),
+    path("/", include("mi_app.urls")),
     
     
 ]
